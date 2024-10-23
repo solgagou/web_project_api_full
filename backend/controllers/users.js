ca2f64721c274d546613673763c57227920792ac
solgagou <solgagou@gmail.com>
2024-10-23 14:33:17 +0200

    feat: add a security solution por the key on user controllers

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -91,10 +91,11 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
+  console.log('Datos recibidos en el login:', { email, password });
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET || 'some-secret-key', { expiresIn: '7d' });
 
       res.send({ token });
     })
